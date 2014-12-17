54d51056b4080ed3a2fb13bfdb8d8c7a3358f604
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2014-12-17 18:48:46 +0100

    Result for 3 digits numbers

--- src/test/scala/pl/scalania/euler/Euler04Spec.scala
+++ src/test/scala/pl/scalania/euler/Euler04Spec.scala
@@ -4,10 +4,15 @@ import org.scalatest.{MustMatchers, FlatSpec}
 
 class Euler04Spec extends FlatSpec with MustMatchers {
 
-  "Largest palindrome product " should "be " in {
+  "Largest palindrome product of 2 digits numbers" should "be " in {
     val result = Euler04.largestPalindrome(2)
     assert(result == 9009)
   }
 
+
+  "Largest palindrome product of 3 digits numbers" should "be " in {
+    val result = Euler04.largestPalindrome(3)
+    assert(result == 580085)
+  }
 
 }
