7ae9a91d508fb30b7f93f98a92c33e8f27fc49d3
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2014-12-19 23:38:51 +0100

    Euler02

---
+++ src/main/scala/pl/scalania/euler/Euler02.scala
@@ -0,0 +1,5 @@
+package pl.scalania.euler
+
+object Euler02 {
+ def sumOfTheEvenValuedFibonacciTermsLessThan(maxFibonacciValue:Long):Long = ???
+}
---
+++ src/test/scala/pl/scalania/euler/Euler02Spec.scala
@@ -0,0 +1,28 @@
+package pl.scalania.euler
+
+class Euler02Spec extends AbstractSpec {
+
+  val FOUR_MILLION  = 4000000
+
+  val TEN = 10
+
+  "Euler02 " should {
+
+    s"Sum of even Fibonacci numbers below $TEN" in {
+      val result = Euler02.sumOfTheEvenValuedFibonacciTermsLessThan(TEN)
+      val expected:Long = ???
+      result should be(19)
+    }
+
+
+    s"Sum of even Fibonacci numbers below $FOUR_MILLION" in {
+      val result = Euler02.sumOfTheEvenValuedFibonacciTermsLessThan(FOUR_MILLION)
+      val expected:Long = ???
+      result should be(expected)
+    }
+
+
+
+  }
+
+}
