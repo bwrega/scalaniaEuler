b62145c283dc06e531110919fd704376b9ef31d9
andrzej.jozwik@gmail.com <andrzej.jozwik@gmail.com>
2014-12-22 11:36:15 +0100

    Euler02

---
+++ src/main/scala/pl/scalania/euler/Euler03.scala
@@ -0,0 +1,5 @@
+package pl.scalania.euler
+
+object Euler03 {
+ def largestPrimeFactor(number:Long):Long = ???
+}
---
+++ src/test/scala/pl/scalania/euler/Euler03Spec.scala
@@ -0,0 +1,26 @@
+package pl.scalania.euler
+
+class Euler03Spec extends AbstractSpec {
+
+  val A  = 13195l
+
+  val B = 600851475143l
+
+  "The largest prime factor of the number " should {
+
+    s" $A" in {
+      val result = Euler03.largestPrimeFactor(A)
+      result should be(29)
+    }
+
+
+    s" $B" in {
+      val result = Euler03.largestPrimeFactor(B)
+      result should be(6857)
+    }
+
+
+
+  }
+
+}
