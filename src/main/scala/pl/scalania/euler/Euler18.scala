d3f004b00dc685e68aaf8adce35ee241c9d3b153
Bartłomiej Wręga <bartlomiej.wrega@decerto.pl>
2014-12-28 03:31:20 +0100

    euler67 done

--- src/main/scala/pl/scalania/euler/Euler18.scala
+++ src/main/scala/pl/scalania/euler/Euler18.scala
@@ -3,25 +3,32 @@ package pl.scalania.euler
 
 object Euler18 {
   def maxTriangle(triangle: List[List[Int]]) = {
-    Position(triangle, 0, 0).max
-  }
+    var valueCache = Map[(Int, Int), Int]()
 
-  case class Position(triangle: List[List[Int]], row: Int, col: Int) {
-    lazy val isLastRow: Boolean = row + 1 == triangle.length
 
-    def left: Option[Position] = if (isLastRow) None else Some(Position(triangle, row + 1, col))
 
-    def right: Option[Position] = if (isLastRow) None else Some(Position(triangle, row + 1, col + 1))
+    case class Position(triangle: List[List[Int]], row: Int, col: Int) {
+      lazy val isLastRow: Boolean = row + 1 == triangle.length
 
-    lazy val value = triangle(row)(col)
-    lazy val max: Int = {
-      val maxLeft: Int = left.map(_.max).getOrElse(0)
-      val maxRight: Int = right.map(_.max).getOrElse(0)
-      if (maxLeft > maxRight)
-        maxLeft + value
-      else
-        maxRight + value
+      def left: Option[Position] = if (isLastRow) None else Some(Position(triangle, row + 1, col))
+
+      def right: Option[Position] = if (isLastRow) None else Some(Position(triangle, row + 1, col + 1))
+
+      lazy val value = triangle(row)(col)
+      lazy val max: Int = {
+        valueCache.getOrElse((row, col), {
+          val maxLeft: Int = left.map(_.max).getOrElse(0)
+          val maxRight: Int = right.map(_.max).getOrElse(0)
+          val ret: Int = if (maxLeft > maxRight)
+            maxLeft + value
+          else
+            maxRight + value
+          valueCache += (row, col) -> ret
+          ret
+        })
+      }
     }
+    Position(triangle, 0, 0).max
   }
 
 }
